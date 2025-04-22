91b3271ecd5e34aa5e17cceb17e7484a3301684a
Tim Baz <timtb.dev@gmail.com>
2025-04-22 05:25:39 -0700

    refactor: enhance BrowserWrapper component with customizable className and improved hover effects for better visual feedback

--- components/project/browser.tsx
+++ components/project/browser.tsx
@@ -1,18 +1,25 @@
+import { cn } from "@/lib/utils";
 import React, { FC } from "react";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-const BrowserWrapper: FC<Props> = ({ children }) => {
+const BrowserWrapper: FC<Props> = ({ children, className }) => {
   return (
-    <div className="bg-accent border-border rounded-t-lg border text-sm">
+    <div
+      className={cn(
+        "group bg-accent rounded-t-lg border border-b-0 text-sm",
+        className,
+      )}
+    >
       <div className="flex gap-2 p-2">
-        <span className="size-3 rounded-full bg-white dark:bg-white/20"></span>
-        <span className="size-3 rounded-full bg-white dark:bg-white/20"></span>
-        <span className="size-3 rounded-full bg-white dark:bg-white/20"></span>
+        <span className="size-3 rounded-full bg-white group-hover:bg-red-500 dark:bg-white/20 dark:group-hover:bg-red-500"></span>
+        <span className="size-3 rounded-full bg-white group-hover:bg-yellow-500 dark:bg-white/20 dark:group-hover:bg-yellow-500"></span>
+        <span className="size-3 rounded-full bg-white group-hover:bg-green-500 dark:bg-white/20 dark:group-hover:bg-green-500"></span>
       </div>
-      <div className="border-border bg-background dark:bg-accent/50 border">
+      <div className="border-border bg-background dark:bg-accent/50 border border-r-0 border-b-0 border-l-0">
         {children}
       </div>
     </div>
